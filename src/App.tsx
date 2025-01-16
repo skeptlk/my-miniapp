561b59fbf15e209dce89550f522ddb42cd4c4013
Bogdan <sceptlc@bk.ru>
2025-01-16 22:21:35 +0300

    User telegram api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { popup, initData } from '@telegram-apps/sdk'
 
 function App() {
+  const user = initData.user();
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to my Telegram app!</h1>
         <img src={logo} className="App-logo" alt="logo" />
+
+        <button onClick={async () => {
+          popup.open({
+            title: `Hello, ${user?.firstName} ${user?.lastName}`,
+            message: `Here is a test message for ${user?.firstName} ${user?.lastName}`,
+            buttons: [{ id: 'my-id', type: 'default', text: 'Default text' }],
+          });
+        }}>Click me!</button>
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { init, backButton } from '@telegram-apps/sdk';
+
+init();
+
+backButton.mount();
+
+const off = backButton.onClick(() => {
+  off();
+  window.history.back();
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
